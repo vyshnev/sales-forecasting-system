2ba007788796086e3bca558d664b0a49cc287556
Vyshnev <vyshnevnandanam@gmail.com>
2025-01-07 17:11:50 +0530

    fixed file not found error

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,57 +1,57 @@
 import pandas as pd
 
 def load_and_merge_data(data_path):
-    """ Loads, preprocesses and merge all the data"""
+    """Loads, preprocesses and merges all data files."""
 
     try:
-        store_df = pd.read_csv(f'{data_path}/store data_set.csv')
-        feature_df = pd.read_csv(f'{data_path}/Features data set.csv')
+        stores_df = pd.read_csv(f'{data_path}/stores data-set.csv')
+        features_df = pd.read_csv(f'{data_path}/Features data set.csv')
         sales_df = pd.read_csv(f'{data_path}/sales data-set.csv')
     except FileNotFoundError:
-        print('Error: Could not find one or more files. Make sure they are in the correct path')
-
+        print("Error: Could not find one or more data files. Make sure they are in the correct folder and named correctly")
         return None
 
-    #Merge the data
-    #Convert the date column to datetime
-    feature_df['Date'] = pd.to_datetime(feature_df['Date'])
-    sales_df['Date'] = pd.to_datetime(sales_df['Date'])
+    # Merge Data
+    # convert to datetime before merging
+    features_df['Date'] = pd.to_datetime(features_df['Date'],format='mixed')
+    sales_df['Date'] = pd.to_datetime(sales_df['Date'], format='mixed')
     # Sum sales across departments and merge
     sales_by_store = sales_df.groupby(['Store', 'Date'])['Weekly_Sales'].sum().reset_index()
-    merged_df = pd.merge(sales_by_store, feature_df, on=['Store', 'Date'], how='left')
-    merged_df = pd.merge(merged_df, store_df, on=['Store'], how='left')
+    merged_df = pd.merge(sales_by_store, features_df, on=['Store', 'Date'], how='left')
+    merged_df = pd.merge(merged_df, stores_df, on=['Store'], how='left')
 
     return merged_df
 
 
-def handle_missing_values(df,method='fillna_0'):
-    """
-    Handling missing values.
+def handle_missing_values(df, method='fillna_0'):
+  """Handles missing values.
     Args:
-        df: Panda DataFrame
-        method: 'fillna_0' or 'mean' or 'meadian
-    """
+        df: Pandas Dataframe
+        method: 'fillna_0' or 'mean' or 'median'
 
-    if method == 'fillna_0':
-        df.fillna(0)
-    elif method == 'mean':
-        df.fillna(df.mean())
-    elif method == 'median':
-        df.fillna(df.median())
-    
-    return df
+  """
+
+  if method == 'fillna_0':
+    df = df.fillna(0)
+  elif method == 'mean':
+    df = df.fillna(df.mean())
+  elif method == 'median':
+        df = df.fillna(df.median())
+
+  return df
+
 
 def handle_categorical_data(df, one_hot_encode=True):
-    """
-    Handles categorical data such as Stroe type
+  """Handles categorical features such as store type
 
-    Args: 
-        df: Panda DataFrame
-        one_hot_encode: boolean, If true, the categorical will be one hot encoded, other wise it will be numerical
-    """
-    if one_hot_encode:
+  Args:
+    df: Pandas Dataframe
+    one_hot_encode: Bool. If true, the categories will be one hot encoded, otherwise it will be numerical
+  """
+
+  if one_hot_encode:
         df = pd.get_dummies(df, columns=['Type'])
-    else:
-        df['Type'] = df['Type'].astype('category').cat.codes #encodes as numerical data
+  else:
+      df['Type'] = df['Type'].astype('category').cat.codes #encode as numeric data
 
-    return df
+  return df
